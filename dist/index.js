1d050462b27c67edfbb0d5104b1be8df0085ef36
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-18 12:47:51 +0300

    Cleanup

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,2 @@
-var t=()=>({name:"astro-networks",hooks:{"astro:config:setup":()=>{}}});export{t as default};
+var t=()=>({name:"astro-networks",hooks:{"astro:config:setup":()=>{}}});export { t as default };
+
