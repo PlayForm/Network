4a3b526b9fcc4415bd24de55b3e4b851a4bf78cb
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-09 15:37:17 +0300

    squash!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import type { AstroIntegration } from "astro";
 
 export default (): AstroIntegration => ({
-	"name": "astro-networks",
-	"hooks": {
+	name: "astro-networks",
+	hooks: {
 		"astro:config:setup": () => {},
 	},
 });
