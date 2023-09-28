3b3ad1366e76ea1aeba575f8151913eb315bc695
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-28 23:29:29 +0300

    squash!

--- Target/Index.js
+++ Target/Index.js
@@ -1,1 +1,8 @@
-var t=()=>({name:"astro-networks",hooks:{"astro:config:setup":()=>{}}});export{t as default};
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = (function () { return ({
+    name: "astro-networks",
+    hooks: {
+        "astro:config:setup": function () { },
+    },
+}); });
